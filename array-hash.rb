9cdc95a392cbd82f32cd62466ed92c3464e37540
Wendell <wencheung@gmail.com>
2017-08-10 18:31:24 +0000

    this is my first commit into github

---
+++ array-hash.rb
@@ -0,0 +1,28 @@
+# ARRAY:
+# Create an array that contains strings of all of your favorite snacks. Store this array in a variable.
+snacks = ["Oreos", "Cheese-Its", "Ben & Jerry", "Yogurt", "chips & guacamole", "brownies"]
+
+# Iterate over your array of snacks to print out: "#{snack} is one of my favorite snacks."
+
+snacks.each do |yumyum|
+    puts "Mmmmmmm #{yumyum} is soooo delicious!!"
+end
+
+
+# Return the number of items in the array.
+
+puts snacks.length
+
+# Return the first item in the array.
+
+# Return the last item in the array.
+
+# HASH:
+
+# Create a hash that describes your house. Store data relating to the number of bedrooms, number of bathrooms, type of house (apartment, condo, townhouse, house, etc), and size of house in square feet. Store your hash in a variable.
+
+# Iterate over your house hash to print out the key value pairs.
+
+# Return the number of bathrooms in the house.
+
+# Iterate over the house hash to print out all the values of the hash.
